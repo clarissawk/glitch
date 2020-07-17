41049a40f9913a7864d10d62d457108252231050
Glitch (hello-webpage) <none>
2020-07-17 03:37:27 +0000

    🤑✋ Checkpoint ./learn-more.html:18579135/7409 ./style.css:18579135/27936 ./DNst.txt:18579135/120356 ./.glitch-assets:18579135/333 ./script.js:18579135/794 ./take-action.css:18579135/10662 ./take-action.html:18579135/4387

--- script.js
+++ script.js
@@ -4,10 +4,6 @@
 
 /* TODO: Add a variable to store the "my-list" element */
 
-
 /* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 
-
 /* TODO: Declare the function checkOffList and add actions inside the { } */
-
-
